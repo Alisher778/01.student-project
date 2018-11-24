f346efa7be9ae6a05e60b34b1b66f518644c6b1a
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-24 19:01:53 +0500

    dom manipulation exercises is hal done

---
+++ 06.DOM/main.js
@@ -0,0 +1,13 @@
+var myId = document.getElementById('myId');
+console.log(myId);
+var className = document.getElementsByClassName('clName');
+console.log(className[0]);
+var p = document.getElementsByTagName('p');
+console.log(p);
+var p2 = document.querySelector('p');
+console.log(p2);
+var pDiv = document.querySelectorAll('div> p');
+console.log(pDiv);
+
+var email = document.querySelector('input[type=email]');
+console.log(email);
