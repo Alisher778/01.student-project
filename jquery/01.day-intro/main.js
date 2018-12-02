4526931dc44abbe47f42f0dd8a39c45f22703f1b
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-02 14:40:24 +0500

    started jquery project

---
+++ jquery/01.day-intro/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    // document.getElementById('btn');
+    // document.querySelector('#btn');
+
+    $('h1').click(function() {
+        $(this).hide()
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
