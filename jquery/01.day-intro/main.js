89f49bb687925adfcc9073e2bd1764d507df360c
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-02 15:13:56 +0500

    work in process

--- jquery/01.day-intro/main.js
+++ jquery/01.day-intro/main.js
@@ -3,11 +3,25 @@ $(document).ready(function() {
     // document.getElementById('btn');
     // document.querySelector('#btn');
 
-    $('h1').click(function() {
-        $(this).hide()
+    $('#hide').click(function() {
+        // $('h1').toggle(2000);
+        // $('h1').fadeOut(2000);
+        // $('h1').fadeToggle(2000);
+        // $('h1').slideUp(800);
+        $('#myDiv').stop()
+    });
+    $('#show').click(function() {
+        // $('h1').show('slow');
+        // $('h1').fadeIn(2000);
+        // $('h1').fadeTo(2000,0.3);
+        // $('h1').slideDown(800);
+
+        $('#myDiv').animate({height: '120px', width: '100%', background: 'green'}, 2000, function() {
+            console.log('finished')
+        })
     });
     
-    
+ 
 
 
 
