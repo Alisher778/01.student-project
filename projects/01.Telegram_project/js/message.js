4676b23aebefb41480a569447b798d04c307d164
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-09 17:26:22 +0500

    finished remove user function

---
+++ projects/01.Telegram_project/js/message.js
@@ -0,0 +1,33 @@
+console.log('messaging');
+var users = JSON.parse(localStorage.data);
+
+function loadUsers() {
+    var userList = document.getElementById('users');
+
+    for(var i=0; i<users.length; i++) {
+        var newUser = 
+         `<li id="user${users[i].id}">
+            <img src="img/user.svg" alt="">
+            <div class="user-info">
+                <h3>${users[i].full_name} by ${users[i].username}</h3>
+            </div>
+            <button id=${users[i].id} onclick="removeUser(this.id)">X</button>
+        </li>`
+        userList.innerHTML += newUser;
+    }
+}
+loadUsers();
+
+function removeUser(id) {
+   var parentElement = document.getElementById('user'+id);
+   parentElement.remove();
+   var localStorageData = JSON.parse(localStorage.data);
+
+   var newLocalstorageData = [];
+   for(var i=0; i<localStorageData.length; i++) {
+        if(localStorageData[i].id != id) {
+            newLocalstorageData.push(localStorageData[i]);
+        }
+   }
+   localStorage.data = JSON.stringify(newLocalstorageData);
+}
