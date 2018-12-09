352c1cffafe58dc558d338d3d242f2f0a0c5e9b9
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-09 16:02:31 +0500

    finished storing authenticated data to telegram

--- projects/01.Telegram_project/js/main.js
+++ projects/01.Telegram_project/js/main.js
@@ -12,14 +12,22 @@ function callNext() {
     var nextel = document.getElementById('next');
     var inputs = pre.children;
     var goNext = true;
+    var obj = {};
     for(var i=0; i<inputs.length; i++) {
         if(inputs[i].nodeName === "INPUT" && !inputs[i].value) {
           goNext = false;
+        } else {
+            if(inputs[i].nodeName === "INPUT") {
+                obj[inputs[i].name] = inputs[i].value;
+            }
         }
     }
+    
+
     if(goNext) {
         pre.style.display = 'none';
         nextel.style.display = 'flex';
+        localStorage.newUser = JSON.stringify(obj);
     }
 }
 
@@ -33,6 +41,21 @@ function goMessage() {
 
     if(myPassword && repeatMyPassword) {
         if(myPassword === repeatMyPassword) {
+            if(!localStorage.data) localStorage.data = "[]";
+            // Get data from localstorage and parse it into javascript object
+            var localStorageData = JSON.parse(localStorage.data);
+            var getNewUser = JSON.parse(localStorage.newUser);
+
+            // Create a new user
+            var newData = Object.assign(getNewUser, {id: localStorageData.length+1 || 1, password: myPassword, createdAt: Date.now()});
+
+            // Store it back into localstorage
+            localStorageData.push(newData);
+            localStorage.data = JSON.stringify(localStorageData);
+            
+            // make newuser from localstorage empty
+            localStorage.newUser = "";
+            
             document.getElementById('repeatMyPassword').style.cssText = 'border: 2px solid; color: initial';
             setTimeout(function() {
                 location.href = '/message.html'
