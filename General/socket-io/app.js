a60eaa360822d23814f90c1742b9bb5623aa4892
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-16 22:34:56 +0500

    added socket.io video stream package

--- General/socket-io/app.js
+++ General/socket-io/app.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
 app.set('view engine', 'ejs');
 
+io.on('connection', (socket) => {
+  console.log(socket);
+  socket.on('send', (data) => {
+    io.emit('all', data);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+})
 app.get('/', (req, res) => {
   res.render('index', {title: 'Socket.io'})
 })
 
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('App is started');
 });
