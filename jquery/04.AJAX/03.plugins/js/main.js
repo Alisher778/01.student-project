a371236124ce92a14d9825391796e9da9ce16a13
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-19 16:05:33 +0500

    added jqueryui

---
+++ jquery/04.AJAX/03.plugins/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#gal').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true
+
+    });
+
+
+
+});
