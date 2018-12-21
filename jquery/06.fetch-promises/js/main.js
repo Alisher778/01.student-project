5217e9e332efe1ee733691b5d74045654149793c
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-21 17:49:24 +0500

    added fetch

---
+++ jquery/06.fetch-promises/js/main.js
@@ -0,0 +1,13 @@
+// then  == success
+// catch == error
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(data) {
+        
+       return data.json();
+    })
+    .then(function(res) {
+        console.log(res)
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
