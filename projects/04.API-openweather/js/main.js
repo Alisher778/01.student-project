0fd8ac533bc72458361247ab69b673262a933739
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-26 15:46:30 +0500

    added new city search

--- projects/04.API-openweather/js/main.js
+++ projects/04.API-openweather/js/main.js
@@ -1,2 +1,32 @@
 var APIKEY = "fb39b298b395c36588f68338fa37815c";
-var URL = "//api.openweathermap.org/data/2.5/weather?q=Tashkent&appid="+APIKEY
+var URL = "//api.openweathermap.org/data/2.5/weather?q=Tashkent&units=metric&appid="+APIKEY
+var icon = "http://openweathermap.org/img/w/";
+fetch(URL)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        
+        console.log(data)
+        var city = document.getElementById('city');
+        city.innerHTML = data.name;
+        city.innerHTML += '<p>'+ data.main.temp +'</p>'
+        city.innerHTML += '<p>'+ data.weather[0].main +'</p>'
+        city.innerHTML += '<img src="'+ icon+ data.weather[0].icon +'.png">'
+        city.innerHTML += '<p>'+ data.wind.speed +'</p>'
+        // city.innerHTML += '<img src="img/'+data.name+'.jpg">'
+    })
+    .catch(function(err) {
+        console.log(err)
+    });
+
+    document.getElementById('search').addEventListener('click', function() {
+        var cityName = document.getElementById('search-city').value;
+
+        
+
+
+    })
+
+
+
