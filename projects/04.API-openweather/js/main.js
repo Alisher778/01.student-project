2358b771df4acf2b7bf96253c3fbb37bafb95473
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-26 16:30:23 +0500

    finishing weather app

--- projects/04.API-openweather/js/main.js
+++ projects/04.API-openweather/js/main.js
@@ -1,5 +1,5 @@
 var APIKEY = "fb39b298b395c36588f68338fa37815c";
-var URL = "//api.openweathermap.org/data/2.5/weather?q=Tashkent&units=metric&appid="+APIKEY
+var URL = "//api.openweathermap.org/data/2.5/forecast?q=Tashkent&units=metric&appid="+APIKEY
 var icon = "http://openweathermap.org/img/w/";
 fetch(URL)
     .then(function(res) {
@@ -22,10 +22,27 @@ fetch(URL)
 
     document.getElementById('search').addEventListener('click', function() {
         var cityName = document.getElementById('search-city').value;
+        var cityUrl = "//api.openweathermap.org/data/2.5/weather?lat=40.12&lon=67.85&units=metric&appid="+APIKEY
 
-        
-
-
+        fetch(cityUrl)
+            .then(function(res) {
+                return res.json()
+            })
+            .then(function(data) {
+                if(data.cod != '404') {
+                    var city = document.getElementById('city');
+                    city.innerHTML = data.name;
+                    city.innerHTML += '<p>'+ data.main.temp +'</p>'
+                    city.innerHTML += '<p>'+ data.weather[0].main +'</p>'
+                    city.innerHTML += '<img src="'+ icon+ data.weather[0].icon +'.png">'
+                    city.innerHTML += '<p>'+ data.wind.speed +'</p>'
+                } else {
+                    console.log(data)
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
     })
 
 
